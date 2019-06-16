de14a2729f6bc362d3cc68033fb18f8e939c7afd
Kewin Brand <leo_name5@hotmail.com>
2019-06-16 18:13:17 -0300

    adicionada cena de gameover. criado controles de restart/gameover

--- Assets/Scripts/Cronometro.cs
+++ Assets/Scripts/Cronometro.cs
@@ -1,30 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class Cronometro : MonoBehaviour
 {
     public Text txtCronometro;
-    private int tempo;
-    public bool vivo;
+    public int tempo;
 
     // Start is called before the first frame update
     void Start()
     {
-        vivo = true;
-        tempo = 60;
+        tempo = 5;
         txtCronometro.text = tempo.ToString();
-        StartCoroutine(StartTimer());
+        StartCoroutine(IniciarContagem());
     }
 
-    private IEnumerator StartTimer()
+    IEnumerator IniciarContagem()
     {
-        while (vivo)
+        while (true)
         {
             yield return new WaitForSeconds(1);
             tempo--;
             txtCronometro.text = tempo.ToString();
+            VerificarSePerdeu();
         }
     }
+
+    private void VerificarSePerdeu()
+    {
+        if (tempo <= 0)
+        {
+            SceneManager.LoadScene("Gameover");
+        }
+    }
+
+    public void AdicionarTempo()
+    {
+        tempo = tempo + 15;
+        txtCronometro.text = tempo.ToString();
+    }
+
+    public void RemoverTempo()
+    {
+        tempo -= 30;
+        txtCronometro.text = tempo.ToString();
+        VerificarSePerdeu();
+    }
+
 }
