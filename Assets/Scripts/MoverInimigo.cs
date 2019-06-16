d3e3c02e9112666c254c9f1f451ffe202d215631
Kewin Brand <leo_name5@hotmail.com>
2019-06-16 11:24:35 -0300

    adicionado nav mesh e script que mexe inimigos em direção ao player

---
+++ Assets/Scripts/MoverInimigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoverInimigo : MonoBehaviour
+{
+
+    private NavMeshAgent navMeshAgent;
+    private Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(UpdatePath());
+    }
+
+    private IEnumerator UpdatePath()
+    {
+        while(target != null)
+        {
+            navMeshAgent.SetDestination(target.position);
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+}
