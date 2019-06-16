4dbc2f185f88b926e4e41c8a4eb02c4c3267862b
Kewin Brand <leo_name5@hotmail.com>
2019-06-16 17:29:35 -0300

    adicionado coletaveis

---
+++ Assets/Scripts/Cronometro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cronometro : MonoBehaviour
+{
+    public Text txtCronometro;
+    private int tempo;
+    public bool vivo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vivo = true;
+        tempo = 60;
+        txtCronometro.text = tempo.ToString();
+        StartCoroutine(StartTimer());
+    }
+
+    private IEnumerator StartTimer()
+    {
+        while (vivo)
+        {
+            yield return new WaitForSeconds(1);
+            tempo--;
+            txtCronometro.text = tempo.ToString();
+        }
+    }
+}
