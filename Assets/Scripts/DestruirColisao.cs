37243d4d8735575da7e43d1abc7bf22e184c3893
Kewin Brand <leo_name5@hotmail.com>
2019-06-17 00:24:08 -0300

    adicao coletaveis

---
+++ Assets/Scripts/DestruirColisao.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirColisao : MonoBehaviour
+{
+    public TipoColetavel tipoColetavel;
+    public ParticleSystem particle;
+
+    public ControleJogo controleJogo;
+    public Mover_Player moverPlayer;
+
+    private AnimationScript animationScript;
+    public float tempoEsperaAntesDestruir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationScript = GetComponent<AnimationScript>();
+        moverPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Mover_Player>();
+        controleJogo = GameObject.FindGameObjectWithTag("GameController").GetComponent<ControleJogo>();
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            switch (tipoColetavel)
+            {
+                case TipoColetavel.Tempo:
+                    controleJogo.AdicionarTempo();
+                    break;
+                case TipoColetavel.Velocidade:
+                    moverPlayer.AdicionarVelocidade();
+                    break;
+            }
+
+            StartCoroutine(Destruir());            
+        }
+    }
+
+    private IEnumerator Destruir()
+    {
+        animationScript.rotationSpeed = 75;
+        particle.Play();
+        yield return new WaitForSeconds(tempoEsperaAntesDestruir);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Mover_Player.cs
+++ Assets/Scripts/Mover_Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Mover_Player : MonoBehaviour
 {
-    public float speed = 6.0F;
+    public float speed = 6;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     public float rotateSpeed = 3.0F;
     private Vector3 moveDirection = Vector3.zero;
+    private float velocidadeTemporaria;
 
     Animator anim;
     int jumpHash;
@@ -17,6 +18,7 @@ public class Mover_Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        velocidadeTemporaria = 0;
         anim = GetComponent<Animator>();
         jumpHash = Animator.StringToHash("Jump");
         runStateHash = Animator.StringToHash("Base Layer.Run");
@@ -45,4 +47,23 @@ public class Mover_Player : MonoBehaviour
         //Rotate Player
         transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
     }
+
+
+    public void AdicionarVelocidade()
+    {
+        velocidadeTemporaria += 10;
+        speed *= 2;
+        StartCoroutine(ControlarVelocidadeTemporaria());
+    }
+
+    private IEnumerator ControlarVelocidadeTemporaria()
+    {
+        while(velocidadeTemporaria >= 0)
+        {
+            yield return new WaitForSeconds(1);
+            velocidadeTemporaria--;
+        }
+        speed = speed / 2;
+    }
+
 }
