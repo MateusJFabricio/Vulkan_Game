a4d0fba7d1b45ca459237b29e96e5e2fe4659c78
Kewin Brand <leo_name5@hotmail.com>
2019-06-17 01:46:10 -0300

    remove tempo se zerg ataca

--- Assets/Scripts/MoverInimigo.cs
+++ Assets/Scripts/MoverInimigo.cs
@@ -8,11 +8,13 @@ public class MoverInimigo : MonoBehaviour
 
     private NavMeshAgent navMeshAgent;
     private Transform target;
-    
+    private ControleJogo controleJogo;
+
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        controleJogo = GameObject.FindGameObjectWithTag("GameController").GetComponent<ControleJogo>();
         navMeshAgent = GetComponent<NavMeshAgent>();
         StartCoroutine(UpdatePath());
     }
@@ -20,8 +22,13 @@ public class MoverInimigo : MonoBehaviour
     private IEnumerator UpdatePath()
     {
         while(target != null)
-        {   
+        {
             navMeshAgent.SetDestination(target.position);
+            if (navMeshAgent.remainingDistance != 0 && navMeshAgent.remainingDistance <= 8)
+            {   
+                controleJogo.RemoverTempo();
+                yield return new WaitForSeconds(1f);
+            }
             yield return new WaitForSeconds(0.25f);
         }
     }
