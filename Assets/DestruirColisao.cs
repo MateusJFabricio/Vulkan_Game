d813cfa084b178f48c335999d7057c83ad8964da
Kewin Brand <leo_name5@hotmail.com>
2019-06-16 23:41:01 -0300

    adicao coletaveis

---
+++ Assets/DestruirColisao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirColisao : MonoBehaviour
+{
+
+    public ParticleSystem particle;
+    private AnimationScript animationScript;
+    public float tempoEsperaAntesDestruir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationScript = GetComponent<AnimationScript>();
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Destruir());            
+        }
+    }
+
+    private IEnumerator Destruir()
+    {
+        animationScript.rotationSpeed = 75;
+        particle.Play();
+        yield return new WaitForSeconds(tempoEsperaAntesDestruir);
+        Destroy(gameObject);
+    }
+}
